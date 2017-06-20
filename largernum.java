76bb0cbb3e8b5b74997d4b22a2b89ec28ab48a8a
kuzhalvaimozhi <kuzhalvaimozhi.b@gmail.com>
2017-06-20 04:17:24 -0700

    Create largernum.java

---
+++ largernum.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KAVITHA
+ */
+public class largernum {
+    public static void main(String[] args)
+    {
+        System.out.println("enter three integers");
+        Scanner s=new Scanner(System.in);
+       
+        int a=s.nextInt();  
+        int b=s.nextInt();
+        int c=s.nextInt();
+         if(a>b)
+            {
+             if(a>c)
+             {
+              System.out.println(a+"is larger ");
+               }
+             else
+             {
+               
+              System.out.println(c+"is larger ");
+              }
+         
+            }
+         else if(b>c)
+        {
+            System.out.println(b+"is larger ");
+                  }
+         else
+         {
+             System.out.println(c+"is larger ");
+             
+         }
+        }
+      
+    }
+
