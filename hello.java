89795495dbb140199ced5e808c9d58875b82fbc0
kuzhalvaimozhi <kuzhalvaimozhi.b@gmail.com>
2017-06-20 04:29:04 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+     for(int i=0;i<5;i++)
+    System.out.println("hello");
+        }
+    
+        }
