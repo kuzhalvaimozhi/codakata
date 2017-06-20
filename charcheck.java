2f889d85827d43c82e123e133c729104d0cd6b3b
kuzhalvaimozhi <kuzhalvaimozhi.b@gmail.com>
2017-06-20 04:04:58 -0700

    Create charcheck.java

---
+++ charcheck.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KAVITHA
+ */
+public class charcheck {
+    public static void main(String[] args)
+    {
+        System.out.println("enter a character");
+        Scanner s=new Scanner(System.in);
+       
+        char a=s.next().charAt(0);
+        if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+        {
+            System.out.println("The entered character is vowel");
+        }
+        else
+        {
+             System.out.println("The entered character is consonant");
+        }
+        }
+      
+    }
+
